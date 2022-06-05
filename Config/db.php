2e7111dbb4227b989e7e7ad870f047fdc0a8481f
hammrxu <hammrxu@gmail.com>
2022-06-05 13:22:08 -0400

    first commit

---
+++ Config/db.php
@@ -0,0 +1,9 @@
+<?php
+		
+			$servername = "localhost";
+			$username = "local_user";
+			$password = "localpassword";
+			$dbname = "mysqlsampledatabase";
+			$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
