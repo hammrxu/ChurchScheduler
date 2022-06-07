03799d1563809b1326d3a75997537b6d31b8874a
hammrxu <hammrxu@gmail.com>
2022-06-07 18:53:13 -0400

    1

--- components/main_table.php
+++ components/main_table.php
@@ -5,8 +5,7 @@
             <tr><td><button onclick="location.href='//phpdemo/pages/index.php'">Home Page</button></td>
                 <td><button onclick="location.href='//phpdemo/pages/FilterSearching.php'">Filter Searching</button></td>
                 <td><button onclick="location.href='//phpdemo/pages/CreateTable.php'">Create Table </button></td>
-                <td><button onclick="location.href='//phpdemo/pages/FilterSearching.php'">Edit Data </button></td>
+                <td><button onclick="location.href='//phpdemo/pages/EditData.php'">Edit Data </button></td>
             </tr>
-            
     </table>
 </div>
