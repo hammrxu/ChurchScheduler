4e876129a93f11070634e0eaad03afd9f2a1791a
hammrxu <hammrxu@gmail.com>
2022-08-03 08:42:52 -0400

    del

--- pages/PlanSchedule.php
+++ pages/PlanSchedule.php
@@ -23,6 +23,9 @@
         require_once("../components/navBar.html");
     ?>
     <iframe name="dummyframe" style="display:none;"></iframe>
+    <pre id="memo">
+*Each role at a time can be assign by ONE GROUP or ONE HELPER.
+If two persons, make them form a group/team at page <a href='../pages/ServiceGroup.php' style='color:orange' >Manage Service Group</a></pre>
     </br>
     
     <!-- developing -->
@@ -193,58 +196,7 @@
 
 
 </script>
-<script>
-    let dragged;
 
-/* events fired on the draggable target */
-document.addEventListener("drag", (event) => {
-  console.log("dragging");
-});
-
-document.addEventListener("dragstart", (event) => {
-  // store a ref. on the dragged elem
-  dragged = event.target;
-  // make it half transparent
-  event.target.classList.add("dragging");
-});
-
-document.addEventListener("dragend", (event) => {
-  // reset the transparency
-  event.target.classList.remove("dragging");
-});
-
-/* events fired on the drop targets */
-document.addEventListener("dragover", (event) => {
-  // prevent default to allow drop
-  event.preventDefault();
-}, false);
-
-document.addEventListener("dragenter", (event) => {
-  // highlight potential drop target when the draggable element enters it
-  if (event.target.classList.contains("dropzone")) {
-    event.target.classList.add("dragover");
-  }
-});
-
-document.addEventListener("dragleave", (event) => {
-  // reset background of potential drop target when the draggable element leaves it
-  if (event.target.classList.contains("dropzone")) {
-    event.target.classList.remove("dragover");
-  }
-});
-
-document.addEventListener("drop", (event) => {
-  // prevent default action (open as link for some elements)
-  event.preventDefault();
-  // move dragged element to the selected drop target
-  if (event.target.classList.contains("dropzone")) {
-    event.target.classList.remove("dragover");
-    dragged.parentNode.removeChild(dragged);
-    event.target.appendChild(dragged);
-  }
-});
-        
-</script>
 <script>
     function func(abb,b){
         console.log(b);
