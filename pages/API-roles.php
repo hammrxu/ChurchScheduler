09eb57c99747fbaa8826c53cb4342660846e76e9
hammrxu <hammrxu@gmail.com>
2022-09-10 10:13:54 -0400

    clean

--- pages/API-roles.php
+++ pages/API-roles.php
@@ -15,31 +15,7 @@ require_once("../components/navBar.php");
 
 <div class="wrapper">
 
-	<div class="top_navbar">
-		<div class="logo">
-			<a href="#">Coding Market</a>
-		</div>
-		<div class="top_menu">
-			<div class="home_link">
-				<a href="#" onclick="window.location.reload();">
-					<span class="icon"><i class="fas fa-home"></i></span>
-					<span>Home</span>
-				</a>
-			</div>
-			<div class="right_info">
-				<div class="icon_wrap">
-					<div class="icon">
-						<i class="fas fa-bell"></i>
-					</div>
-				</div>
-				<div class="icon_wrap">
-					<div class="icon">
-						<i class="fas fa-cog"></i>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+
 
 	<div class="main_body">
 
